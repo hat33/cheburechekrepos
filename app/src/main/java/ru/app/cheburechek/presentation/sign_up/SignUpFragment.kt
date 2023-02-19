82916f11742970e60d8490eeb5449249e98d6df5
Anna Krylova <apgavrilyuk@gmail.com>
2023-02-19 17:48:10 +0300

    rf: переименовала sign up fragment

--- app/src/main/java/ru/app/cheburechek/presentation/sign_up/SignUpFragment.kt
+++ app/src/main/java/ru/app/cheburechek/presentation/sign_up/SignUpFragment.kt
@@ -3,13 +3,13 @@ package ru.app.cheburechek.presentation.sign_up
 import androidx.fragment.app.viewModels
 import ru.app.cheburechek.R
 import ru.app.cheburechek.core.base.ui.BaseFragment
-import ru.app.cheburechek.databinding.FragmentSignupBinding
+import ru.app.cheburechek.databinding.FragmentSignUpBinding
 
-class SignUpFragment : BaseFragment<FragmentSignupBinding>(R.layout.fragment_signup) {
+class SignUpFragment : BaseFragment<FragmentSignUpBinding>(R.layout.fragment_sign_up) {
 
     override val viewModel: SignUpViewModel by viewModels()
 
-    override fun FragmentSignupBinding.initialize() {
+    override fun FragmentSignUpBinding.initialize() {
         this.vm = viewModel
         this.fragmentSignupToolbar.setNavigationOnClickListener {
             viewModel.onCloseClick()
